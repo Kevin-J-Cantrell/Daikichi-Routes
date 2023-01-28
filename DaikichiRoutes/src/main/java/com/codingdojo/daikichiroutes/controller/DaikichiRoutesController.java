3d62f339393daf01ca5487e92cb905baf266278b
Kevin J Cantrell <112343044+Kevin-J-Cantrell@users.noreply.github.com>
2023-01-27 19:03:08 -0800

    second commit

--- DaikichiRoutes/src/main/java/com/codingdojo/daikichiroutes/controller/DaikichiRoutesController.java
+++ DaikichiRoutes/src/main/java/com/codingdojo/daikichiroutes/controller/DaikichiRoutesController.java
@@ -1,7 +1,6 @@
 package com.codingdojo.daikichiroutes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,22 +22,4 @@ public class DaikichiRoutesController {
 	public String third() {
 		return "tomorrow, an opportunity will arise, so be sure to be open to ne ideas!";
 	}
-	
-	@GetMapping("/travel/{location}")
-	public String showLesson(@PathVariable("location") String location){
-		return "Congratulations you will soon trvel to " + location ;
-		
-	}
-	@GetMapping("/lotto/{num}")
-	    public String showLesson(@PathVariable("num") int num){
-		String result = "";
-		if(num %2 == 0 ) {
-			result = "You will take a grand journey in the near future, but be weary of tempting offers";
-		}
-		else {
-			result = "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
-		}
-	    	return result;
-	    																							//text that says 'You will take a grand
-	   }
 }
